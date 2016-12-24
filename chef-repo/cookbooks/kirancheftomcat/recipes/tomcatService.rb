da2275d73b87f769f4c107eb7ad3b1819a493008
kdasari2208 <dasari1410@yahoo.com>
2016-12-24 20:22:26 +0530

    files added to transfer to remote

---
+++ chef-repo/cookbooks/kirancheftomcat/recipes/tomcatService.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: kirancheftomcat
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+service 'tomcat' do
+  action :start
+end
