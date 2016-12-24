eb6c9d37bcc30d09a629df53aa5e6c87affab796
kdasari2208 <dasari1410@yahoo.com>
2016-12-24 18:11:22 +0530

    Changes made to tomcat file

---
+++ chef-repo/cookbooks/kirancheftomcat/recipes/tomcat.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: kirancheftomcat
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+apt-package "tomcat7" do
+action :install
+end
